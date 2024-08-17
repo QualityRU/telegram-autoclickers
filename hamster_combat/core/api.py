74e3d30be1bc3dcb18f6fe7b3ed87e7e40c20299
Quality <qualityru@gmail.com>
2024-08-17 06:36:03 +0300

    Update

--- hamster_combat/core/api.py
+++ hamster_combat/core/api.py
@@ -13,6 +13,7 @@ from config import (
     telegramBotLogging,
 )
 from core.logger import log
+from core.promogames import *
 from core.utilities import (
     CalculateCardProfitCoefficient,
     DailyCipherDecode,
@@ -20,51 +21,6 @@ from core.utilities import (
     TextToMorseCode,
     number_to_string,
 )
-
-SupportedPromoGames = {
-    '43e35910-c168-4634-ad4f-52fd764a843f': {
-        'name': 'Bike Ride 3D in Hamster FAM',
-        'appToken': 'd28721be-fd2d-4b45-869e-9f253b554e50',
-        'promoId': '43e35910-c168-4634-ad4f-52fd764a843f',
-        'delay': 100,
-        'retry_delay': 20,
-    },
-    'fe693b26-b342-4159-8808-15e3ff7f8767': {
-        'name': 'My Clone Army',
-        'appToken': '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
-        'promoId': 'fe693b26-b342-4159-8808-15e3ff7f8767',
-        'delay': 100,
-        'retry_delay': 20,
-    },
-    'b4170868-cef0-424f-8eb9-be0622e8e8e3': {
-        'name': 'Chain Cube 2024',
-        'appToken': 'd1690a07-3780-4068-810f-9b5bbf2931b2',
-        'promoId': 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
-        'delay': 100,
-        'retry_delay': 20,
-    },
-    'c4480ac7-e178-4973-8061-9ed5b2e17954': {
-        'name': 'Train Miner',
-        'appToken': '82647f43-3f87-402d-88dd-09a90025313f',
-        'promoId': 'c4480ac7-e178-4973-8061-9ed5b2e17954',
-        'delay': 100,
-        'retry_delay': 20,
-    },
-    'dc128d28-c45b-411c-98ff-ac7726fbaea4': {
-        'name': 'Merge Away',
-        'appToken': '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
-        'promoId': 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
-        'delay': 100,
-        'retry_delay': 20,
-    },
-    '61308365-9d16-4040-8bb0-2f4a4c69074c': {
-        'name': 'Twerk Race 3D',
-        'appToken': '61308365-9d16-4040-8bb0-2f4a4c69074c',
-        'promoId': '61308365-9d16-4040-8bb0-2f4a4c69074c',
-        'delay': 100,
-        'retry_delay': 20,
-    },
-}
 
 
 class HamsterKombatAccount:
@@ -102,33 +58,12 @@ class HamsterKombatAccount:
         ):
             return
 
-        if (
-            level not in telegramBotLogging['messages']
-            or telegramBotLogging['messages'][level] is False
-        ):
-            return
-
-        requests.get(
-            f"https://api.telegram.org/bot{telegramBotLogging['bot_token']}/sendMessage?chat_id={self.telegram_chat_id}&text={message}"
-        )
-
-    def SendTelegramLog(self, message, level='other_errors'):
-        if (
-            not telegramBotLogging['is_active']
-            or self.telegram_chat_id == ''
-            or telegramBotLogging['bot_token'] == ''
-        ):
-            return
-
-        if (
-            level not in telegramBotLogging['messages']
-            or telegramBotLogging['messages'][level] is False
-        ):
-            return
-
-        requests.get(
-            f"https://api.telegram.org/bot{telegramBotLogging['bot_token']}/sendMessage?chat_id={self.telegram_chat_id}&text={message}"
-        )
+        try:
+            requests.get(
+                f"https://api.telegram.org/bot{telegramBotLogging['bot_token']}/sendMessage?chat_id={self.telegram_chat_id}&text={message}"
+            )
+        except Exception as e:
+            log.error(f'[{self.account_name}] TelegramLog error: {e}')
 
     # Send HTTP requests
     def HttpRequest(
@@ -811,7 +746,7 @@ class HamsterKombatAccount:
 
         if AccountConfigData['dailyKeysMiniGame']['isClaimed'] == True:
             log.info(
-                f'[{self.account_name}] Daily keys mini game already claimed.'
+                f'\033[1;34m[{self.account_name}] Daily keys mini game already claimed.\033[0m'
             )
             return
 
@@ -864,7 +799,7 @@ class HamsterKombatAccount:
 
         if response['dailyKeysMiniGame']['isClaimed'] == True:
             log.info(
-                f'[{self.account_name}] Daily keys mini game already claimed.'
+                f'\033[1;34m[{self.account_name}] Daily keys mini game already claimed.\033[0m'
             )
             return
 
@@ -1132,13 +1067,16 @@ class HamsterKombatAccount:
 
             self.HttpRequest(url, headers_option, 'OPTIONS', 204, True)
 
-            payload = json.dumps(
-                {
-                    'promoId': promoData['promoId'],
-                    'eventId': eventID,
-                    'eventOrigin': 'undefined',
-                }
-            )
+            PayloadData = {
+                'promoId': promoData['promoId'],
+                'eventId': eventID,
+                'eventOrigin': 'undefined',
+            }
+
+            if 'eventType' in promoData and promoData['eventType'] != None:
+                PayloadData['eventType'] = promoData['eventType']
+
+            payload = json.dumps(PayloadData)
 
             response = self.HttpRequest(
                 url, headers, 'POST', 200, payload, True
@@ -1235,7 +1173,7 @@ class HamsterKombatAccount:
                 and state['receiveKeysToday'] >= promo['keysPerDay']
             ):
                 log.info(
-                    f"[{self.account_name}] Playground game {SupportedPromoGames[promo['promoId']]['name']} already claimed."
+                    f"\033[1;34m[{self.account_name}] Playground game {SupportedPromoGames[promo['promoId']]['name']} already claimed.\033[0m"
                 )
                 return False
 
